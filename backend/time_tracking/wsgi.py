a4c18c26a36e2bf30d2ab099b84bb5d56a1a60cd
koooommmm <nico.pika17@gmail.com>
2023-06-10 00:11:15 +0900

    add: リンターとフォーマッターの設定

--- backend/time_tracking/wsgi.py
+++ backend/time_tracking/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'time_tracking.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "time_tracking.settings")
 
 application = get_wsgi_application()
